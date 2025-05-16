57f54b6d8d3c3bff0993a9940d82b8141c4b55c6
Aditi Singh <aditisingh01299@gmail.com>
2025-05-16 17:54:45 +0530

    lecture7

---
+++ lecture7/accessing children of elements/script.js
@@ -0,0 +1,6 @@
+console.log(document.body.firstChild) //gives first child
+console.log(document.body.lastChild) //gives last child
+console.log(document.body.childNodes) // gives the name of all children (form of node list)
+
+let arr = Array.from(document.body.childNodes) // gives the name of all children (form of array)
+console.log(arr) 
