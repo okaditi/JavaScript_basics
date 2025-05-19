8872a5d399e7908a6464fa9b371686825b940b2a
Aditi Singh <aditisingh01299@gmail.com>
2025-05-19 22:15:34 +0530

    lecture7

--- lecture7/v34_element only navigation/script.js
+++ lecture7/v34_element only navigation/script.js
@@ -1,2 +1,7 @@
+const changeBodyRed = () =>{
+    document.body.firstElementChild.style.background = "red"
+}
+
 let b = document.body
 console.log("First Child of body is:",b.firstChild)
+console.log("First Element Child of body is:",b.firstElementChild)
