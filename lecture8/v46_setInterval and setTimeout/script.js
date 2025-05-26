7d936e76e78ddb7e4b8439aafd6b68ef85decdf0
Aditi Singh <aditisingh01299@gmail.com>
2025-05-26 20:47:23 +0530

    lecture8

--- lecture8/v46_setInterval and setTimeout/script.js
+++ lecture8/v46_setInterval and setTimeout/script.js
@@ -1,5 +1,27 @@
-alert("Hello")
-setTimeout(function(){
-    alert("This is set timeout")
-})
+document.write("Hello")
 
+const sum = (a,b) => {
+    console.log("Yes i m running",a+b)
+    a+b
+}
+
+setTimeout(sum,5000,1,2)
+
+// setInterval(function(){
+//     alert("SetInterval")
+// },3000)
+
+// let a = setTimeout(function(){   
+//     alert("This is set timeout")
+// },2000) //time is in ms
+
+// let b = prompt("Do you want to run the settimeout?")
+// if ("n" == b){
+//     clearTimeout(a)
+// }
+
+
+// clearInterval(time) => will stop showing the alert after this time
+
+
+// console.log(a) //a stores time id
