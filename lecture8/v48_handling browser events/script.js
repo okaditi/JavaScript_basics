6e5dbff81def759956dc0e1c1b746ccc19e49d91
Aditi Singh <aditisingh01299@gmail.com>
2025-06-02 16:53:49 +0530

    lecture8

--- lecture8/v48_handling browser events/script.js
+++ lecture8/v48_handling browser events/script.js
@@ -1,3 +1,14 @@
-BigInt.addEventListner('click',function(e){
-    alert("Hello World!")
+btn.addEventListner('click',function(e){
+    alert("Hello World1!")
 })
+
+btn.addEventListner('click',function(e){
+    alert("Hello World2!")
+})
+
+let a = prompt("What is your favorite number?");
+if (a==2){
+    btn.removeEventListner('click',function(e){
+        alert("Hello World!")
+    })
+}
