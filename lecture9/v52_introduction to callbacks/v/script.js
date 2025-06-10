81d4fc2f22e3ceb19950aa62d7511aecf5063856
Aditi Singh <aditisingh01299@gmail.com>
2025-06-11 01:39:07 +0530

    commit

--- lecture9/v52_introduction to callbacks/v/script.js
+++ lecture9/v52_introduction to callbacks/v/script.js
@@ -1,5 +1,20 @@
-let a = prompt("What is your name?");
-let b = prompt("What is your age?");
-let c = prompt("What is your favorite color?");
+// // Syncronous programming
+// let a = prompt("What is your name?");
+// let b = prompt("What is your age?");
+// let c = prompt("What is your favorite color?");
 
-console.log(a," is ",b," years old and has ",c," favorite color.");
+// console.log(a," is ",b," years old and has ",c," favorite color.");
+
+// // Asynchronous Programming
+// setTimeout(function(){
+//     console.log("Hey i am good")
+// },3000)
+// console.log("End")
+
+//callbacks
+function loadScript(src){
+    var script = document.createElement("script");
+    script.src = src;
+    document.body.appendChild(script);
+}
+
